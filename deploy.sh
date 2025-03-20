e0cb66127e6755c7cb316effcf4000cd071fa746
Sudharsunm <126581024+Sudharsunm@users.noreply.github.com>
2025-03-20 13:34:15 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u sudharsunm -p dckr_pat_OVW1mupsA4HIpYZF6p5aTjFOirI
+    sudo docker tag test sudharsunm/dev
+    sudo docker push sudharsunm/dev
+    
     
