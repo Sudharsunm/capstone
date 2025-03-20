e6765c360310e03df510e85c25bd6618f8f25ad5
Sudharsunm <126581024+Sudharsunm@users.noreply.github.com>
2025-03-20 14:00:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u sudharsunm -p dckr_pat_OVW1mupsA4HIpYZF6p5aTjFOirI
-    sudo docker tag test1 sudharsunm/dev
-    sudo docker push sudharsunm/dev
+    docker tag test1 sudharsunm/dev
+    docker push sudharsunm/dev
     
     
